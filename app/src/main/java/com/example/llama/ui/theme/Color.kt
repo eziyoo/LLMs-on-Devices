c46edd54d055d6ebba02a87731851c5727a7097f
M Ehsani <40179494+ehsaani@users.noreply.github.com>
2025-07-07 18:36:06 +0200

    Delete app directory

--- app/src/main/java/com/example/llama/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.llama.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val UserBubbleColor = Color(0xFFDCF8C6)        // Green
-val UserTextColor = Color(0xFF000000)          // Black
-
-val ModelBubbleColor = Color(0xFFECECEC)       // Light gray
-val ModelTextColor = Color(0xFF000000)         // Black
